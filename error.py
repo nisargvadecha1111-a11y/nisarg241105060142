6c8c95894bf9c6b89fdb105518fced15681698ed
nisargvadecha1111-a11y <nisargvadecha1111@gmail.com>
2026-02-23 06:02:14 -0500

    Add files via upload

---
+++ error.py
@@ -0,0 +1,19 @@
+Print(“Variance Estimator”)
+       N = int(input(“Enter number of values: “))
+I = 0
+       Total = 0
+While i < n:
+       Value = int(input(“Enter value: “))
+Total = total + value
+       I = i + 1
+Mean = total / n
+       I = 0
+Variance = 0
+       While i < n:
+         Variance = variance + (value – mean) ** 2
+                 I = i + 1
+         Variance = variance / n
+                Print(“Mean:”, mean)
+        Print(“Variance:”, variance
+                If variance < 0:
+       Print(“Impossible variance”)
---
+++ solution.py
@@ -0,0 +1,25 @@
+Print(“Variance Estimator”)
+
+N = int(input(“Enter number of values: “))
+
+I = 0
+Total = 0
+Values = []
+
+While i < n:
+    Value = int(input(“Enter value: “))
+    Total = total + value
+    Values.append(value)
+    I = i + 1
+
+
+Mean = total / n
+
+
+I = 0
+Variance = 0
+While i < n:
+    Variance = variance + (values[i] – mean) ** 2
+    I = i + 1
+
+Variance = variance / n
